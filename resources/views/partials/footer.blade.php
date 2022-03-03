3d987fed4586dcfe13223f7e80d92833c33d88bb
Cezar <cezarlantosgmail.com>
2022-03-03 19:49:02 +0100

    add footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,106 @@
+<div class="footer-nav">
+    <ul>
+        <li>
+            <img src="{{ asset('images/buy-comics-digital-comics.png') }}" alt="">
+            <span>DIGITAL COMICS</span>
+        </li>
+        <li>
+            <img src="{{ asset('images/buy-comics-merchandise.png') }}" alt="">
+            <span>DC MERCHANDISE</span>
+        </li>
+        <li>
+            <img src="{{ asset('images/buy-comics-subscriptions.png') }}" alt="">
+            <span>SUBSCRIPTION</span>
+        </li>
+        <li>
+            <img src="{{ asset('images/buy-comics-shop-locator.png') }}" alt="">
+            <span>COMIC SHOP LOCATOR</span>
+        </li>
+        <li>
+            <img src="{{ asset('images/buy-dc-power-visa.svg') }}" alt="">
+            <span>DC POWER VISA</span>
+        </li>
+    </ul>
+</div>
+
+<div class="footer-app">
+    <div class="list">
+        <ul>
+            <li><h2>DC COMICS</h2></li>
+            <li>Characters</li>
+            <li>Comics</li>
+            <li>Movies</li>
+            <li>TV</li>
+            <li>Games</li>
+            <li>Videos</li>
+            <li>News</li>
+            <li><h2>SHOP</h2></li>
+            <li>Shop DC</li>
+            <li>Shop DC Collectibles</li>
+        </ul>
+    </div>
+
+    <div class="list">
+        <ul>
+            <li><h2>DC</h2></li>
+            <li>Terms Of Use</li>
+            <li>Privacy policy (New)</li>
+            <li>Ad Choices</li>
+            <li>Advertising</li>
+            <li>Jobs</li>
+            <li>Subscriptions</li>
+            <li>Talent Workshops</li>
+            <li>CPSC Certificates</li>
+            <li>Rating</li>
+            <li>Shop Help</li>
+            <li>Contact Us</li>
+        </ul>
+    </div>
+
+    <div class="list">
+        <ul>
+            <li><h2>SITES</h2></li>
+            <li>DC</li>
+            <li>MAD Magazine</li>
+            <li>DC Kids</li>
+            <li>DC Universe</li>
+            <li>DC Power Visa</li>
+        </ul>
+    </div>
+    {{-- <span>All Site Content TM and © 2020 DC Entertainment, unless otherwise <span>noted here</span>. All rights reserved</span>
+    <br>
+    <span>Cookies Settings</span> --}}
+
+    <div class="logo"></div>
+</div>
+
+<div class="rights">
+    <p>All Site Content TM and © 2020 DC Entertainment, unless otherwise <span class="lighted">noted here</span>. All rights reserved</p>
+    <span>Cookies Settings</span>
+</div>
+{{-- <div>All Site Content TM and © 2020 DC Entertainment, unless otherwise <span class="lighted">noted here</span>. All rights reserved</div>
+<div class="cookie">Cookies Settings</div> --}}
+
+<div class="footer-sign-up"> 
+    <button>SIGN-UP NOW!</button>
+    <nav>
+        <ul>
+            <li>FOLLOW US</li>
+            <li>
+                <img src="{{ asset('images/footer-facebook.png') }}">
+            </li>
+            <li>
+                <img src="{{ asset('images/footer-twitter.png') }}">
+            </li>
+            <li>
+                <img src="{{ asset('images/footer-youtube.png') }}">
+            </li>
+            <li>
+                <img src="{{ asset('images/footer-pinterest.png') }}">
+            </li>
+            <li>
+                <img src="{{ asset('images/footer-periscope.png') }}">
+            </li>
+        </ul>
+    </nav>
+</div>
