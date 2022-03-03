27dc48915848dd9061e255ae2615e86121281d90
Cezar <cezarlantosgmail.com>
2022-03-03 18:54:08 +0100

    add jumbo + cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,25 @@
 @section('pageTitle', 'Homepage')
 
 @section('content')
+
+    <div class="jumbo"></div>
+
+    <main>
+        <button class="button">CURRENT SERIES</button>
+
+        <div class="container-card">
+            @foreach($data as $value)
+
+                <div class="card-dc">
+                    <img src="{{ $value['thumb'] }}" alt="">
+                    <p>{{ $value['series'] }}</p>
+                </div>
+
+            @endforeach
+            
+            <button class="button-load">LOAD MORE</button>
+        </div>  
+        
+    </main>
     
 @endsection
